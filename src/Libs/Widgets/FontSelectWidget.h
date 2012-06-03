33b75dd84b29992041f45ddf4d6082aac9619a43
Sascha Cunz <Sascha@BabbelBox.org>
2012-06-03 12:29:11 +0200

    Libs: Add Config + Widgets Subdirs

---
+++ src/Libs/Widgets/FontSelectWidget.h
@@ -0,0 +1,59 @@
+/*
+ * MacGitver
+ * Copyright (C) 2012 Sascha Cunz <sascha@babbelbox.org>
+ *
+ * This program is free software; you can redistribute it and/or modify it under the terms of the
+ * GNU General Public License (Version 2) as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
+ * even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with this program; if
+ * not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+#ifndef MGV_FONT_SELECT_WIDGET_H
+#define MGV_FONT_SELECT_WIDGET_H
+
+#include "Widgets/WidgetsApi.h"
+
+#include <QWidget>
+#include <QFontComboBox>
+
+class QCheckBox;
+class QComboBox;
+
+class WIDGETS_API FontSelectWidget : public QWidget
+{
+	Q_OBJECT
+public:
+	FontSelectWidget( QWidget* parent = NULL );
+	~FontSelectWidget();
+
+public:
+	void setFontName( const QString& fontName );
+	void setFontSize( int size );
+	void setBold( bool v );
+	void setItalic( bool v );
+
+	bool bold() const;
+	bool italic() const;
+	QString fontName() const;
+	int fontSize() const;
+
+	void setFontFilters( QFontComboBox::FontFilters filters );
+	QFontComboBox::FontFilters fontFilters() const;
+
+private slots:
+	void onCurrentFontChanged( QFont font );
+
+private:
+	QFontComboBox*		mcboFontName;
+	QCheckBox*			mchkBold;
+	QCheckBox*			mchkItalic;
+	QComboBox*			mcboSize;
+};
+
+#endif
---
+++ src/Libs/Widgets/WidgetsApi.h
@@ -0,0 +1,28 @@
+/*
+ * MacGitver
+ * Copyright (C) 2012 Sascha Cunz <sascha@babbelbox.org>
+ *
+ * This program is free software; you can redistribute it and/or modify it under the terms of the
+ * GNU General Public License (Version 2) as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
+ * even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with this program; if
+ * not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+#ifndef MGV_WIDGETS_API_H
+#define MGV_WIDGETS_API_H
+
+#include <qglobal.h>
+
+#ifdef Widgets_EXPORTS
+#	define WIDGETS_API Q_DECL_EXPORT
+#else
+#	define WIDGETS_API Q_DECL_IMPORT
+#endif
+
+#endif

1541adc236937b4dd930af28ac972a8b9a397327
Sascha Cunz <Sascha@BabbelBox.org>
2012-07-16 01:39:09 +0200

    ModLogging: Prepare a new empty module for log output; required as it's already referenced from .Gui.xml

---
+++ src/Modules/Logging/LoggingModule.cpp
@@ -0,0 +1,54 @@
+/*
+ * MacGitver
+ * Copyright (C) 2012 Sascha Cunz <sascha@babbelbox.org>
+ *
+ * This program is free software; you can redistribute it and/or modify it under the terms of the
+ * GNU General Public License (Version 2) as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
+ * even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with this program; if
+ * not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+#include <QtPlugin>
+
+#include "Interfaces/IMainWindow.h"
+
+#include "LoggingModule.h"
+#include "LoggingView.h"
+
+LoggingModule::LoggingModule()
+{
+}
+
+void LoggingModule::setupConfigPages( IConfigDialog* dialog )
+{
+}
+
+Module::Types LoggingModule::providesModuleTypes() const
+{
+	return View;
+}
+
+Heaven::View* LoggingModule::createLoggingView()
+{
+	return new LoggingView();
+}
+
+void LoggingModule::initialize()
+{
+	registerView( "Log",
+				  Heaven::GlobalViewType,
+				  &LoggingModule::createLoggingView );
+}
+
+void LoggingModule::deinitialize()
+{
+	unregisterView( "Log" );
+}
+
+Q_EXPORT_PLUGIN2( Logging, LoggingModule )
---
+++ src/Modules/Logging/LoggingView.h
@@ -0,0 +1,30 @@
+/*
+ * MacGitver
+ * Copyright (C) 2012 Sascha Cunz <sascha@babbelbox.org>
+ *
+ * This program is free software; you can redistribute it and/or modify it under the terms of the
+ * GNU General Public License (Version 2) as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
+ * even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with this program; if
+ * not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+#ifndef MGV_LOGGIN_VIEW_H
+#define MGV_LOGGING_VIEW_H
+
+#include "Heaven/Views/GlobalView.h"
+
+class LoggingView : public Heaven::GlobalView
+{
+	Q_OBJECT
+public:
+	LoggingView();
+};
+
+#endif
+

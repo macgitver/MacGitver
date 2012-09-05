d4e24a16e87a99265978166d32b2dcd7a2ef2694
Sascha Cunz <Sascha@BabbelBox.org>
2012-09-06 00:50:11 +0200

    Main: Set configured font

--- src/Main/MainWindow.cpp
+++ src/Main/MainWindow.cpp
@@ -61,9 +61,9 @@ MainWindow::MainWindow()
 		}
 	}
 
-	QVariant v = Config::self().get( "Main/Font", QFont() );
-	QFont f = v.value< QFont >();
-	setFont( f );
+	QFont font = Config::defaultFont();
+	setFont( font );
+	QApplication::setFont( font );
 }
 
 MainWindow::~MainWindow()

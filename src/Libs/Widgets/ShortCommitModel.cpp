fe7bc9c2ff9217a4babb52fb56b5147fcd8bbcb3
Sascha Cunz <Sascha@BabbelBox.org>
2012-09-18 03:17:55 +0200

    Widgets: Add a model for looking up commit objects

---
+++ src/Libs/Widgets/ShortCommitModel.cpp
@@ -0,0 +1,121 @@
+/*
+ * MacGitver
+ * Copyright (C) 2012 Sascha Cunz <sascha@babbelbox.org>
+ *
+ * This program is free software; you can redistribute it and/or modify it under the terms of the
+ * GNU General Public License (Version 2) as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
+ * even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with this program; if
+ * not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+#include "GitWrap/Repository.h"
+#include "GitWrap/Result.h"
+
+#include "ShortCommitModel.h"
+
+ShortCommitModel::ShortCommitModel( QObject* parent )
+	: QAbstractTableModel( parent )
+{
+}
+
+ShortCommitModel::~ShortCommitModel()
+{
+}
+
+void ShortCommitModel::clear()
+{
+	beginResetModel();
+
+	mCommitList.clear();
+
+	endResetModel();
+}
+
+void ShortCommitModel::setCommitList( const QList< Git::ObjectCommit >& list )
+{
+	beginResetModel();
+
+	mCommitList = list;
+
+	endResetModel();
+}
+
+bool ShortCommitModel::setCommitList( Git::Repository repo, const QList< Git::ObjectId >& list )
+{
+	QList< Git::ObjectCommit > commits;
+
+	Git::Result r;
+
+	foreach( Git::ObjectId sha1, list )
+	{
+		Git::ObjectCommit commit = repo.lookupCommit( sha1, r );
+		if( !r )
+		{
+			return false;
+		}
+		commits.append( commit );
+	}
+
+	setCommitList( commits );
+	return true;
+}
+
+Git::ObjectCommit ShortCommitModel::indexToCommit( const QModelIndex& index ) const
+{
+	if( index.isValid() )
+	{
+		return mCommitList.at( index.row() );
+	}
+	return Git::ObjectCommit();
+}
+
+int ShortCommitModel::rowCount( const QModelIndex& parent ) const
+{
+	return parent.isValid() ? 0 : mCommitList.count();
+}
+
+int ShortCommitModel::columnCount( const QModelIndex& parent ) const
+{
+	return parent.isValid() ? 0 : 3;
+}
+
+QVariant ShortCommitModel::data( const QModelIndex& index, int role ) const
+{
+	QVariant value;
+	Git::Result r;
+
+	if( role == Qt::DisplayRole )
+	{
+		Git::ObjectCommit commit = indexToCommit( index );
+		switch( index.column() )
+		{
+		case 0:
+			value = commit.shortMessage( r );
+			break;
+
+		case 1:
+			value = commit.author( r ).when().toString();
+			break;
+
+		case 2:
+			value = commit.id( r ).toString().left( 7 );
+			break;
+
+		default:
+			return QVariant();
+		}
+	}
+
+	if( !r )
+	{
+		return QVariant();
+	}
+
+	return value;
+}
---
+++ src/Libs/Widgets/ShortCommitModel.h
@@ -0,0 +1,50 @@
+/*
+ * MacGitver
+ * Copyright (C) 2012 Sascha Cunz <sascha@babbelbox.org>
+ *
+ * This program is free software; you can redistribute it and/or modify it under the terms of the
+ * GNU General Public License (Version 2) as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
+ * even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with this program; if
+ * not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+#ifndef MGV_SHORT_COMMIT_MODEL_H
+#define MGV_SHORT_COMMIT_MODEL_H
+
+#include "Widgets/WidgetsApi.h"
+
+#include <QAbstractItemModel>
+
+#include "GitWrap/ObjectCommit.h"
+
+class WIDGETS_API ShortCommitModel : public QAbstractTableModel
+{
+	Q_OBJECT
+public:
+	explicit ShortCommitModel( QObject* parent = 0 );
+	~ShortCommitModel();
+
+public:
+	void clear();
+	void setCommitList( const QList< Git::ObjectCommit >& list );
+	bool setCommitList( Git::Repository repo, const QList< Git::ObjectId >& list );
+
+public:
+	Git::ObjectCommit indexToCommit( const QModelIndex& index ) const;
+
+public:
+	int rowCount( const QModelIndex& parent ) const;
+	int columnCount( const QModelIndex& parent ) const;
+	QVariant data( const QModelIndex& index, int role ) const;
+
+private:
+	QList< Git::ObjectCommit >	mCommitList;
+};
+
+#endif

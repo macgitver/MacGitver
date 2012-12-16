49caf30ff11985cbe855afcb76ae3a8339cb4436
Sascha Cunz <sascha@babbelbox.org>
2012-12-16 14:26:04 +0100

    Add functions for creating frameworks

--- cmake/Utils.cmake
+++ cmake/Utils.cmake
@@ -19,3 +19,32 @@ MACRO( SET_OUTPUT_PATHS )
     SET( EXECUTABLE_OUTPUT_PATH ${CMAKE_BINARY_DIR}/bin )
     SET( LIBRARY_OUTPUT_PATH    ${CMAKE_BINARY_DIR}/bin )
 ENDMACRO()
+
+# Turn the target ${_target} into a framework.
+#
+FUNCTION( SET_FRAMEWORK _target )
+
+    SET_TARGET_PROPERTIES( ${_target}
+        PROPERTIES  FRAMEWORK TRUE
+    )
+
+ENDFUNCTION()
+
+# Turn the target ${_target} into a framework and install it.
+#
+# On MacOSX, do nothing but invoke SET_FRAMEWORK - BundleFrameworks will copy it at the right
+# localtions. On other Plattforms install Runtime to bin and Library to lib for component
+# ${_component}
+FUNCTION( INSTALL_FRAMEWORK _target _component )
+
+    SET_FRAMEWORK( ${_target} )
+
+    IF( NOT APPLE )
+        INSTALL(
+            TARGETS     ${_target}
+            RUNTIME     DESTINATION bin COMPONENT ${_component}
+            LIBRARY     DESTINATION lib COMPONENT ${_component}
+        )
+    ENDIF()
+
+ENDFUNCTION()

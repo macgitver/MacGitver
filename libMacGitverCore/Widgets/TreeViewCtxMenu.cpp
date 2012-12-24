1f499364470b8c9446c07510235d0799533c9dfe
Sascha Cunz <sascha@babbelbox.org>
2012-12-24 09:43:13 +0100

    TreeView that support ctxmenus out of the box

---
+++ libMacGitverCore/Widgets/TreeViewCtxMenu.cpp
@@ -0,0 +1,39 @@
+/*
+ * MacGitver
+ * Copyright (C) 2012 Sascha Cunz <sascha@babbelbox.org>
+ *
+ * This program is free software; you can redistribute it and/or modify it under the terms of the
+ * GNU General Public License (Version 2) as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
+ * even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with this program; if
+ * not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+#include <QContextMenuEvent>
+
+#include "TreeViewCtxMenu.hpp"
+
+TreeViewCtxMenu::TreeViewCtxMenu( QWidget* parent )
+    : QTreeView( parent )
+{
+}
+
+void TreeViewCtxMenu::contextMenuEvent( QContextMenuEvent* ev )
+{
+    if( ev->reason() == QContextMenuEvent::Keyboard )
+    {
+        emit contextMenu( QModelIndex(), ev->globalPos() );
+        ev->accept();
+    }
+    else if( ev->reason() == QContextMenuEvent::Mouse )
+    {
+        QModelIndex idx = indexAt( ev->pos() );
+        emit contextMenu( idx, ev->globalPos() );
+        ev->accept();
+    }
+}
---
+++ libMacGitverCore/Widgets/TreeViewCtxMenu.hpp
@@ -0,0 +1,37 @@
+/*
+ * MacGitver
+ * Copyright (C) 2012 Sascha Cunz <sascha@babbelbox.org>
+ *
+ * This program is free software; you can redistribute it and/or modify it under the terms of the
+ * GNU General Public License (Version 2) as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
+ * even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with this program; if
+ * not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+#ifndef MGV_CORE_TREE_VIEW_CTXMENU_HPP
+#define MGV_CORE_TREE_VIEW_CTXMENU_HPP
+
+#include <QTreeView>
+
+#include "libMacGitverCore/MacGitverApi.hpp"
+
+class MGV_CORE_API TreeViewCtxMenu : public QTreeView
+{
+    Q_OBJECT
+public:
+    TreeViewCtxMenu( QWidget* parent = NULL );
+
+protected:
+    void contextMenuEvent( QContextMenuEvent* ev );
+
+signals:
+    void contextMenu( const QModelIndex& index, const QPoint& globalPos );
+};
+
+#endif

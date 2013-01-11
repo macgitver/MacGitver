5cd4a960b448ad93a1afd67e610a8a49a8e8e4b4
Sascha Cunz <sascha@babbelbox.org>
2013-01-11 11:43:51 +0100

    Update Copyright year

--- libMacGitverCore/Config/Ui/ConfigDialog.hpp
+++ libMacGitverCore/Config/Ui/ConfigDialog.hpp
@@ -1,6 +1,6 @@
 /*
  * MacGitver
- * Copyright (C) 2012 Sascha Cunz <sascha@babbelbox.org>
+ * Copyright (C) 2012-2013 Sascha Cunz <sascha@babbelbox.org>
  *
  * This program is free software; you can redistribute it and/or modify it under the terms of the
  * GNU General Public License (Version 2) as published by the Free Software Foundation.

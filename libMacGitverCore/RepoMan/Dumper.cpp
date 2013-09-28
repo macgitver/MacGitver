a7dccf9ff5cecd525ae5ab8962d4c295be8a7a79
Sascha Cunz <sascha@babbelbox.org>
2013-09-28 16:57:48 +0100

    Add Dumper class

---
+++ libMacGitverCore/RepoMan/Dumper.cpp
@@ -0,0 +1,57 @@
+/*
+ * MacGitver
+ * Copyright (C) 2012-2013 The MacGitver-Developers <dev@macgitver.org>
+ *
+ * (C) Sascha Cunz <sascha@macgitver.org>
+ * (C) Cunz RaD Ltd.
+ *
+ * This program is free software; you can redistribute it and/or modify it under the terms of the
+ * GNU General Public License (Version 2) as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
+ * even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with this program; if
+ * not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+#include <QStringBuilder>
+
+#include "Dumper.hpp"
+
+namespace RM
+{
+
+    namespace Internal
+    {
+
+        Dumper::Dumper()
+        {
+            level = 0;
+        }
+
+        void Dumper::addLine(const QString& text)
+        {
+            outString += QString(2 * level, QChar(L' ')) % text % QChar(L'\n');
+        }
+
+        void Dumper::indent()
+        {
+            level++;
+        }
+
+        void Dumper::dedent()
+        {
+            --level;
+        }
+
+        QString Dumper::output() const
+        {
+            return outString;
+        }
+
+    }
+
+}
---
+++ libMacGitverCore/RepoMan/Dumper.hpp
@@ -0,0 +1,56 @@
+/*
+ * MacGitver
+ * Copyright (C) 2012-2013 The MacGitver-Developers <dev@macgitver.org>
+ *
+ * (C) Sascha Cunz <sascha@macgitver.org>
+ * (C) Cunz RaD Ltd.
+ *
+ * This program is free software; you can redistribute it and/or modify it under the terms of the
+ * GNU General Public License (Version 2) as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
+ * even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with this program; if
+ * not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+#ifndef MGV_CORE_REPOMAN_DUMPER_HPP
+#define MGV_CORE_REPOMAN_DUMPER_HPP
+
+#include <QStringList>
+#include <QVector>
+
+#include "libMacGitverCore/MacGitverApi.hpp"
+
+namespace RM
+{
+
+    namespace Internal
+    {
+
+        class MGV_CORE_API Dumper
+        {
+        public:
+            Dumper();
+
+        public:
+            void addLine(const QString& line);
+            void indent();
+            void dedent();
+
+        public:
+            QString output() const;
+
+        private:
+            QString outString;
+            int level;
+        };
+
+    }
+
+}
+
+#endif

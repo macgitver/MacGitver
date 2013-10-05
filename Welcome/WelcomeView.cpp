92ddb69be7ab5ce6913c1b3a24208d64d9799186
Nils Fenner <nilsfenner@web.de>
2013-10-05 22:26:59 +0200

    show background logo and gradient in welcome screen
    Note: Because QTextDocument lacks some css support, the logo cannot be centered. Instead I'm using Qt stylesheets to achieve that. The outcommented code shows how it should be done.

--- Welcome/WelcomeView.cpp
+++ Welcome/WelcomeView.cpp
@@ -28,9 +28,28 @@ WelcomeView::WelcomeView()
     mBrowser->setFrameShape( QFrame::NoFrame );
     mBrowser->setOpenExternalLinks( true );
 
+    // NOTE: Set qss style because QTextDocument doesn't support background-repeat.
+    QString css = QString::fromUtf8(
+                "background-image: url(':/ModWelcome/images/bg_logo.png');"
+                "background-attachment: scroll;"
+                "background-repeat: no-repeat;"
+                "background-position: center center;"
+                "background-color: qlineargradient( x1:0 y1:0, x2:0 y2:1, stop:0 #FFFFFF, stop:.25 #CBD0D8, stop:.5 #BBC0CA, stop:.75 #CBD0D8, stop:1 #FFFFFF );"
+                );
+    mBrowser->setStyleSheet( css );
+
+    QTextDocument *doc = new QTextDocument;
+//    doc->addResource( QTextDocument::ImageResource, QUrl( QLatin1String("images/bg_logo.png") ), QPixmap( QLatin1String(":/ModWelcome/images/bg_logo.png") ) );
+
+//    QFile css( QLatin1String( ":/ModWelcome/format.css" ) );
+//    css.open( QFile::ReadOnly );
+//    doc->addResource( QTextDocument::StyleSheetResource, QUrl( QLatin1String("format.css") ), css.readAll() );
+
     QFile f( QLatin1String( ":/ModWelcome/Welcome.html" ) );
     f.open( QFile::ReadOnly );
-    mBrowser->setHtml( QString::fromUtf8( f.readAll().constData() ) );
+    doc->setHtml( QString::fromUtf8( f.readAll().constData() ) );
+
+    mBrowser->setDocument( doc );
 
     setWidget( mBrowser );
 }

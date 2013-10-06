c88b5821d9d6300ca8b437975510b88a0e137505
Nils Fenner <nilsfenner@web.de>
2013-10-06 18:41:02 +0200

    use QWebView instead of QTextBrowser for the welcome screen

--- Welcome/WelcomeView.cpp
+++ Welcome/WelcomeView.cpp
@@ -14,8 +14,10 @@
  *
  */
 
-#include <QTextBrowser>
+#include <QDesktopServices>
 #include <QFile>
+#include <QUrl>
+#include <QWebView>
 
 #include "WelcomeView.h"
 
@@ -24,34 +26,18 @@ WelcomeView::WelcomeView()
 {
     setViewName( trUtf8( "Welcome" ) );
 
-    mBrowser = new QTextBrowser;
-    mBrowser->setFrameShape( QFrame::NoFrame );
-    mBrowser->setOpenExternalLinks( true );
+    mBrowser = new QWebView;
+    mBrowser->page()->setLinkDelegationPolicy( QWebPage::DelegateExternalLinks );
+    connect( mBrowser, SIGNAL(linkClicked(QUrl)), this, SLOT(onLinkClicked(const QUrl&)) );
 
-    // NOTE: Set qss style because QTextDocument doesn't support background-repeat.
-    QString css = QString::fromUtf8(
-                "background-image: url(':/ModWelcome/images/bg_logo.png');"
-                "background-attachment: scroll;"
-                "background-repeat: no-repeat;"
-                "background-position: center center;"
-                "background-color: qlineargradient( x1:0 y1:0, x2:0 y2:1, stop:0 #FFFFFF, stop:.25 #CBD0D8, stop:.5 #BBC0CA, stop:.75 #CBD0D8, stop:1 #FFFFFF );"
-                );
-    mBrowser->setStyleSheet( css );
-
-    QTextDocument *doc = new QTextDocument;
-//    doc->addResource( QTextDocument::ImageResource, QUrl( QLatin1String("images/bg_logo.png") ), QPixmap( QLatin1String(":/ModWelcome/images/bg_logo.png") ) );
-
-//    QFile css( QLatin1String( ":/ModWelcome/format.css" ) );
-//    css.open( QFile::ReadOnly );
-//    doc->addResource( QTextDocument::StyleSheetResource, QUrl( QLatin1String("format.css") ), css.readAll() );
-
-    QFile f( QLatin1String( ":/ModWelcome/Welcome.html" ) );
-    f.open( QFile::ReadOnly );
-    doc->setHtml( QString::fromUtf8( f.readAll().constData() ) );
-
-    mBrowser->setDocument( doc );
+    mBrowser->load( QUrl( QLatin1String("qrc:/ModWelcome/Welcome.html") ) );
 
     setWidget( mBrowser );
+}
+
+void WelcomeView::onLinkClicked(const QUrl &url)
+{
+    QDesktopServices::openUrl( url );
 }
 
 

11810353bb212aec9bc52fb515825a15bd807126
Sascha Cunz <sascha@babbelbox.org>
2013-10-19 03:28:01 +0100

    Adjust RCC-Macro to workaround CMake 2.8.12 changes
    Actually, this uses the unmodified CMake Macro on 2.8.12 and uses
    our improved one with prior version of CMake. This is not ideal, but
    changing established macros isn't either. I'd bet that if you'd ask
    those guys, they wouldn't bother a second to argue that these macros
    are meant to be private...

--- cmake/Qt4.cmake
+++ cmake/Qt4.cmake
@@ -47,39 +47,49 @@ ENDMACRO( QT_UIC )
 
 
 MACRO(QT_RCC infiles outfiles )
+    IF(CMAKE_VERSION VERSION_LESS 2.8.12)
 
-    QT4_EXTRACT_OPTIONS(rcc_files rcc_options ${ARGN})
+        # For CMake < 2.8.12 we're using our modified version of QT4_ADD_RESOURCES, which is able
+        # to return a list of the resource files included in the .qrc file and thus enables us to
+        # show these files in QtCreator or Visual Studio.
 
-    FOREACH (it ${rcc_files})
-        GET_FILENAME_COMPONENT(outfilename ${it} NAME_WE)
-        GET_FILENAME_COMPONENT(infile ${it} ABSOLUTE)
-        GET_FILENAME_COMPONENT(rc_path ${infile} PATH)
-        SET(outfile ${CMAKE_CURRENT_BINARY_DIR}/qrc_${outfilename}.cxx)
+        QT4_EXTRACT_OPTIONS(rcc_files rcc_options ${ARGN})
+        FOREACH (it ${rcc_files})
+            GET_FILENAME_COMPONENT(outfilename ${it} NAME_WE)
+            GET_FILENAME_COMPONENT(infile ${it} ABSOLUTE)
+            GET_FILENAME_COMPONENT(rc_path ${infile} PATH)
+            SET(outfile ${CMAKE_CURRENT_BINARY_DIR}/qrc_${outfilename}.cxx)
 
-        FILE(READ "${infile}" _RC_FILE_CONTENTS)
-        STRING(REGEX MATCHALL "<file[^<]+" _RC_FILES "${_RC_FILE_CONTENTS}")
-        SET(_RC_DEPENDS)
-        FOREACH(_RC_FILE ${_RC_FILES})
-            STRING(REGEX REPLACE "^<file[^>]*>" "" _RC_FILE "${_RC_FILE}")
-            IF(NOT IS_ABSOLUTE "${_RC_FILE}")
-                SET(_RC_FILE "${rc_path}/${_RC_FILE}")
-            ENDIF(NOT IS_ABSOLUTE "${_RC_FILE}")
-            LIST( APPEND ${infiles} ${_RC_FILE} )
-            SET(_RC_DEPENDS ${_RC_DEPENDS} "${_RC_FILE}")
-        ENDFOREACH(_RC_FILE)
+            FILE(READ "${infile}" _RC_FILE_CONTENTS)
+            STRING(REGEX MATCHALL "<file[^<]+" _RC_FILES "${_RC_FILE_CONTENTS}")
+            SET(_RC_DEPENDS)
+            FOREACH(_RC_FILE ${_RC_FILES})
+                STRING(REGEX REPLACE "^<file[^>]*>" "" _RC_FILE "${_RC_FILE}")
+                IF(NOT IS_ABSOLUTE "${_RC_FILE}")
+                    SET(_RC_FILE "${rc_path}/${_RC_FILE}")
+                ENDIF(NOT IS_ABSOLUTE "${_RC_FILE}")
+                LIST( APPEND ${infiles} ${_RC_FILE} )
+                SET(_RC_DEPENDS ${_RC_DEPENDS} "${_RC_FILE}")
+            ENDFOREACH(_RC_FILE)
 
-        QT4_MAKE_OUTPUT_FILE("${infile}" "" "qrc.depends" out_depends)
-        CONFIGURE_FILE("${infile}" "${out_depends}" COPY_ONLY)
+            QT4_MAKE_OUTPUT_FILE("${infile}" "" "qrc.depends" out_depends)
+            CONFIGURE_FILE("${infile}" "${out_depends}" COPY_ONLY)
 
-        ADD_CUSTOM_COMMAND(OUTPUT ${outfile}
-            COMMAND ${QT_RCC_EXECUTABLE}
-            ARGS ${rcc_options} -name ${outfilename} -o ${outfile} ${infile}
-            MAIN_DEPENDENCY ${infile}
-            DEPENDS ${_RC_DEPENDS} "${out_depends}" VERBATIM)
+            ADD_CUSTOM_COMMAND(OUTPUT ${outfile}
+                COMMAND ${QT_RCC_EXECUTABLE}
+                ARGS ${rcc_options} -name ${outfilename} -o ${outfile} ${infile}
+                MAIN_DEPENDENCY ${infile}
+                DEPENDS ${_RC_DEPENDS} "${out_depends}" VERBATIM)
 
-        SET(${outfiles} ${${outfiles}} ${outfile})
+            SET(${outfiles} ${${outfiles}} ${outfile})
+        ENDFOREACH(it)
 
-    ENDFOREACH (it)
+    ELSE()
+        # For CMake >= 2.8.12, use the stock macro from CMake. This will of course leave the infiles
+        # variable empty and resources will not show up in MSVC or QtCreator.
+
+        QT4_ADD_RESOURCES(${outfiles} ${ARGN})
+    ENDIF()
 
 ENDMACRO(QT_RCC)
 

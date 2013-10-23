8a6eab054d3d5f2371625df3b833d0bc1386c565
Sascha Cunz <sascha@babbelbox.org>
2013-10-23 23:43:45 +0100

    Fix copyright headers

--- Logging/LoggingView.hpp
+++ Logging/LoggingView.hpp
@@ -1,6 +1,9 @@
- /*
+/*
  * MacGitver
- * Copyright (C) 2012 Sascha Cunz <sascha@babbelbox.org>
+ * Copyright (C) 2012-2013 The MacGitver-Developers <dev@macgitver.org>
+ *
+ * (C) Sascha Cunz <sascha@macgitver.org>
+ * (C) Cunz RaD Ltd.
  *
  * This program is free software; you can redistribute it and/or modify it under the terms of the
  * GNU General Public License (Version 2) as published by the Free Software Foundation.

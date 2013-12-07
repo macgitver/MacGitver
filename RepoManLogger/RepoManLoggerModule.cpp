78402d37efbe330ab66aa7f2b6eec25a2a381aa0
Sascha Cunz <sascha@babbelbox.org>
2013-12-07 16:33:17 +0000

    Allow to load RepoManLogger with Qt4

--- RepoManLogger/RepoManLoggerModule.cpp
+++ RepoManLogger/RepoManLoggerModule.cpp
@@ -17,6 +17,8 @@
  *
  */
 
+#include <QtPlugin>
+
 #include "RepoManLoggerModule.hpp"
 
 #include "Listener.hpp"
@@ -90,3 +92,6 @@ void RepoManLoggerModule::setupListener()
     listener = new Listener(repoManChannel);
 }
 
+#if QT_VERSION < 0x050000
+Q_EXPORT_PLUGIN2(RepoManLogger, RepoManLoggerModule)
+#endif

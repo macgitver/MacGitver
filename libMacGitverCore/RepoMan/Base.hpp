0332e4817751bdf96907d7483171a25877a2a296
Nils Fenner <nilsfenner@web.de>
2015-02-04 00:16:12 +0100

    minor cleanup

--- libMacGitverCore/RepoMan/Base.hpp
+++ libMacGitverCore/RepoMan/Base.hpp
@@ -40,25 +40,30 @@ namespace RM
     {
         InvalidObject,
 
-        RepoManagerObject,
-        RepoObject,
-        RefObject,
-        RemoteObject,
-        TagObject,
-        BranchObject,
-        RefTreeNodeObject,
+        CollectionNodeObject,
         NamespaceObject,
+        RepoManagerObject,
+
+        // repositories
+        RepoObject,
+        RemoteObject,
         SubmoduleObject,
-        RefLogObject,
-        CollectionNodeObject
+
+        // references
+        BranchObject,
+        RefObject,
+        RefTreeNodeObject,
+        TagObject,
+
+        RefLogObject
     };
 
     enum CollectionTypes
     {
         ctBranches,
-        ctTags,
         ctNamespaces,
-        ctNotes
+        ctNotes,
+        ctTags
     };
 
     class Repo;

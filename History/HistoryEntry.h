bf613c88b8968a6b6d9af9b94f525908cb45d26d
Nils Fenner <nilsfenner@web.de>
2015-02-03 14:22:51 +0100

    History-Module: added "less than" comparator for "inline references"
    Also added the mIsDetached flag, that will be used in upcoming commits.

--- History/HistoryEntry.h
+++ History/HistoryEntry.h
@@ -61,9 +61,86 @@ struct HistoryInlineRef
     bool        mIsRemote   : 1;
     bool        mIsTag      : 1;
     bool        mIsStash    : 1;
+    bool        mIsDetached : 1;
+
+    bool operator ==(const HistoryInlineRef& other) const
+    {
+        if ( this == &other ) {
+            return true;
+        }
+
+        return  mRefName    == other.mRefName       &&
+                mRefName    == other.mRefName       &&
+                mIsBranch   == other.mIsBranch      &&
+                mIsCurrent  == other.mIsCurrent     &&
+                mIsRemote   == other.mIsRemote      &&
+                mIsTag      == other.mIsTag         &&
+                mIsStash    == other.mIsStash       &&
+                mIsDetached == other.mIsDetached    ;
+    }
+
+    inline bool operator !=(const HistoryInlineRef& other) const {
+        return !( *this == other );
+    }
 };
 
-typedef QVector< HistoryInlineRef > HistoryInlineRefs;
+typedef QList< HistoryInlineRef > HistoryInlineRefs;
+
+struct HistoryInlineRef_LessThan
+{
+    inline bool detachedHEAD(const HistoryInlineRef& ref) const
+    {
+        return ref.mIsDetached && !(ref.mIsTag || ref.mIsBranch || ref.mIsStash);
+    }
+
+    inline bool nameLessThan(const HistoryInlineRef& a, const HistoryInlineRef& b) const
+    {
+        return a.mRefName < b.mRefName;
+    }
+
+    bool operator ()(const HistoryInlineRef& a, const HistoryInlineRef& b) const
+    {
+        // sort order:
+        // - tag
+        // - detached HEAD
+        // - current branch
+        // - local branch
+        // - remote branch
+        // - stash
+        // - Everything else is sorted by mRefName
+
+        if ( a.mIsTag ) {
+            // tags have highest priority
+            return b.mIsTag ? nameLessThan(a, b) : true;
+        }
+
+        if ( detachedHEAD( a ) )
+        {
+            // a detached HEAD comes right after tag ? -> There can only be one!
+            return !b.mIsTag;
+        }
+
+        if ( a.mIsBranch )
+        {
+            if ( b.mIsTag || b.mIsCurrent || detachedHEAD( b ) )
+                return false;
+
+            if ( a.mIsCurrent ) {
+                // a is the current branch
+                return true;
+            }
+
+            return ( a.mIsRemote == b.mIsRemote ) ? nameLessThan( a, b ) : !a.mIsRemote;
+        }
+
+        if ( a.mIsStash ) {
+            return b.mIsStash ? nameLessThan(a, b) : !(b.mIsTag || b.mIsBranch || detachedHEAD( b ));
+        }
+
+        return !(b.mIsTag || b.mIsBranch || b.mIsStash || detachedHEAD( b )) ? nameLessThan( a, b ) : false;
+    }
+};
+
 
 class HistoryEntry
 {

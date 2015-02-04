ae2c3332f1e6280a4618d055412ffe2e1cba0833
Nils Fenner <nilsfenner@web.de>
2015-02-04 02:18:45 +0100

    HistoryModel: changed back stash status to "isCurrent", as it contains the most recent stash

--- History/HistoryModel.cpp
+++ History/HistoryModel.cpp
@@ -359,7 +359,7 @@ void HistoryModel::scanInlineReferences()
         else if (ref == QLatin1Literal("refs/stash")) {
             inlRef.mRefName = tr( "<recent stash>" );
             inlRef.mIsBranch = false;
-            inlRef.mIsCurrent = false;
+            inlRef.mIsCurrent = true;
             inlRef.mIsRemote = false;
             inlRef.mIsTag = false;
             inlRef.mIsStash = true;

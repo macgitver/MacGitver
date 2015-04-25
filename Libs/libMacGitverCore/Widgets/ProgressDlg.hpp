d014c4a3afce3053bb731bb72d7f626b2a1497e0
Nils Fenner <nilsfenner@web.de>
2015-04-25 17:09:52 +0200

    copied progress dialog from "Repository" module (ProgressDlg) to libMacGitverCore with required minor changes

---
+++ Libs/libMacGitverCore/Widgets/ProgressDlg.hpp
@@ -0,0 +1,45 @@
+
+#pragma once
+
+#include "libMacGitverCore/MacGitverApi.hpp"
+#include "libBlueSky/Dialog.hpp"
+
+namespace Ui
+{
+    class ProgressDlg;
+};
+
+class MGV_CORE_API ProgressDlg : public BlueSky::Dialog
+{
+    Q_OBJECT
+public:
+    ProgressDlg();
+    ~ProgressDlg();
+
+public:
+    void setAction( const QString& action, const QStringList& open,
+                    const QStringList& current, const QStringList& done );
+    void setCurrent(QObject* current);
+
+private slots:
+    void transportProgress( quint32 totalObjects, quint32 indexedObjects,
+                            quint32 receivedObjects, quint64 receivedBytes );
+    void remoteMessage( const QString& msg );
+
+public:
+    void setDone();
+    void beginStep( const QString& step );
+    void finalizeStep();
+
+protected:
+    void closeEvent( QCloseEvent* ev );
+
+private:
+    Ui::ProgressDlg*    ui;
+
+private:
+    bool            mDone;
+    QString         mBaseLog;
+    QObject*        mCurrent;
+    QString         mRawRemoteMessage;
+};

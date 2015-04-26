bf7ceff2996945682c5e0a74c0f0d30c142ba483
Sascha Cunz <sascha@cunz-rad.com>
2015-04-26 21:33:04 +0100

    Core files for libActivities

---
+++ Libs/libActivities/Api.hpp
@@ -0,0 +1,28 @@
+/*
+ * MacGitver
+ * Copyright (C) 2012-2015 The MacGitver-Developers <dev@macgitver.org>
+ *
+ * (C) Sascha Cunz <sascha@cunz-rad.com>
+ * (C) Cunz RaD Ltd.
+ *
+ * This program is free software; you can redistribute it and/or modify it under the terms of the
+ * GNU General Public License (Version 2) as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
+ * even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with this program; if
+ * not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+#pragma once
+
+#include <QtGlobal>
+
+#ifdef Activities_EXPORTS
+#   define ACTIVITIES_API Q_DECL_EXPORT
+#else
+#   define ACTIVITIES_API Q_DECL_IMPORT
+#endif

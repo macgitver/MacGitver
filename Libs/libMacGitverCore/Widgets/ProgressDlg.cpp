8ee8f51149130bdebd72408834bc112becebb870
Nils Fenner <nilsfenner@web.de>
2015-04-27 13:17:21 +0200

    ProgressDlg: moved initializers back to c'tor

--- Libs/libMacGitverCore/Widgets/ProgressDlg.cpp
+++ Libs/libMacGitverCore/Widgets/ProgressDlg.cpp
@@ -19,6 +19,8 @@ namespace Private
 
     public:
         ProgressWdgt(const QString& description)
+            : mStatus(Running)
+            , mPercentage(0.)
         {
             setupUi(this);
             progressBar->setMinimum(0);
@@ -28,9 +30,9 @@ namespace Private
 
     public:
         Steps   mSteps;
-        Status  mStatus     = Running;
+        Status  mStatus;
 
-        qreal   mPercentage = 0.;
+        qreal   mPercentage;
     };
 
 }

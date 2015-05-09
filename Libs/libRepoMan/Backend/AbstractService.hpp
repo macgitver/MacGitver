c635fcc33b63b333d7376b9aa78151120c7fcccf
Sascha Cunz <sascha@cunz-rad.com>
2015-05-09 21:36:29 +0100

    Add Service Runner into the backend

---
+++ Libs/libRepoMan/Backend/AbstractService.hpp
@@ -0,0 +1,49 @@
+/*
+ * MacGitver
+ * Copyright (C) 2012-2015 The MacGitver-Developers <dev@macgitver.org>
+ *
+ * (C) Sascha Cunz <sascha@cunz-rad.com>
+ * (C) Cunz RaD Ltd.
+ *
+ * This program is free software; you can redistribute it and/or modify it under the terms of the
+ * GNU General Public License (Version 2) as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
+ * even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with this program; if
+ * not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+#pragma once
+
+#include "libGitWrap/Result.hpp"
+
+#include <QObject>
+
+namespace RM { namespace Backend {
+
+class AbstractService
+        : public QObject
+{
+    Q_OBJECT
+public:
+    AbstractService();
+
+signals:
+    void started();
+    void finished();
+
+public:
+    void execute();
+
+protected:
+    virtual void run() = 0;
+
+protected:
+    void logGitError(const Git::Result& r, const char* msg = nullptr);
+};
+
+} }
